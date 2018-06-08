a20e0c139b373cdec205081d0de53202c04eb5da
Francisco Antonio Pérez Lozano <36158124+franperez022@users.noreply.github.com>
2018-06-08 11:07:39 +0200

    Add files via upload
    modificacion del ejercicio 4 para hacerlo con funciones

---
+++ 4funciones.py
@@ -0,0 +1,43 @@
+#4. Pedir por teclado un nombre de coche y te muestre el año de fabricacion
+
+import json
+
+from pprint import pprint
+with open("coches.json") as data_file:
+	data=json.load(data_file)
+opcion=0
+
+def menu (opcion):
+	print("Año de fabricación.")
+	print("---------------------")
+	print("1. Nombre de coche.")
+	print("2. No tengo ni idea.")
+	print("3. Salir.")
+	opcion = int(input("Dime una opción correcta. "))
+	return opcion
+while True:
+	opcion=menu(opcion)
+	if opcion == 1:
+		coche=input("Dime un nombre de coche. ")
+		for año in data:
+			if año["Name"]==coche:
+				print("---------------------")
+				print("El coche elegido tiene la siguiente fecha de fabricacion: ",año["Year"])
+				print("---------------------")
+
+	elif opcion == 2:
+		print("---------------------")
+		print("Te muestro una ayuda.")
+		print("---------------------")
+		for nombre in data:
+			print("*******************")
+			print("Nombre y modelo: ",nombre["Name"])
+
+	elif opcion == 3:
+		
+		print("---------------------")
+		print("Adios")
+		print("---------------------")
+		break
+	else:
+		print("Introduce una opcion valida")
